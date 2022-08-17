c18267fc330ab2f0ea3985fe792384c2e7c76071
HyeonJu <hhjc017@gmail.com>
2022-08-17 15:12:10 +0900

    feat : firebase 로그인

---
+++ src/services/firebase/auth.ts
@@ -0,0 +1,44 @@
+/* eslint-disable prettier/prettier */
+import {
+    AuthProvider,
+    GithubAuthProvider,
+    GoogleAuthProvider,
+    signInWithPopup,
+} from "firebase/auth";
+import { auth } from "./config";
+
+export type ProviderName = "Google" | "Github";
+
+export const firebaseLogin = async (providerName: ProviderName) => {
+    let authProvider;
+
+    switch (providerName) {
+        case "Google":
+            authProvider = new GoogleAuthProvider();
+            break;
+        case "Github":
+            authProvider = new GithubAuthProvider();
+            break;
+        default:
+            break;
+    }
+
+    try {
+        await signInWithPopup(auth, authProvider as AuthProvider);
+        localStorage.setItem("uid", JSON.stringify((auth.currentUser?.uid)));
+        // localStorage.setItem("expiresIn", '');
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const firebaseLogout = async () => {
+    try {
+        if (!auth.currentUser) return;
+        await auth.signOut();
+
+    } catch (error) {
+        console.log(error);
+    }
+
+};
