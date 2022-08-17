dc4696c3ffbb29318ea51695b80838e1ef22edb2
HyeonJu <hhjc017@gmail.com>
2022-08-17 19:10:25 +0900

    design : LoginForm 스타일링

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,22 +1,30 @@
 import React, { useContext } from "react";
+import { FcGoogle as GoogleIcon } from "react-icons/fc";
+import { FaGithub as GithubIcon } from "react-icons/fa";
 import AuthContext from "../../context/Auth";
+import styles from "./LoginForm.module.css";
 
 function LoginForm() {
-  const { login, logout, isLoggedin } = useContext(AuthContext);
+  const { login, isLoggedin } = useContext(AuthContext);
   return (
-    <section>
-      {isLoggedin && (
-        <button type="button" onClick={() => logout()}>
-          Logout
-        </button>
-      )}
+    <section className={styles.loginForm}>
       {!isLoggedin && (
         <>
-          <button type="button" onClick={() => login("Google")}>
-            Google
+          <h1>Sign in</h1>
+          <button
+            type="button"
+            className={`${styles.google} ${styles.login}`}
+            onClick={() => login("Google")}
+          >
+            <GoogleIcon className={styles.icon} size={25} /> With Google
           </button>
-          <button type="button" onClick={() => login("Github")}>
-            Github
+          <button
+            type="button"
+            className={`${styles.github} ${styles.login}`}
+            onClick={() => login("Github")}
+          >
+            <GithubIcon className={styles.icon} size={25} color="#242930" />{" "}
+            With Github
           </button>
         </>
       )}
