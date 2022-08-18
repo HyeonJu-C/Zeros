9224b9c6133f5ec98f8e0e82708e2d7f00be3be8
HyeonJu <hhjc017@gmail.com>
2022-08-18 11:40:25 +0900

    chore : 브랜치 충돌

--- src/pages/Goals.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Goals() {
-  return <p>goals page</p>;
-}
-
-export default Goals;
--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -1,16 +1,25 @@
 import React, { useContext } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Landing from "../pages/Landing";
-import Goals from "../pages/Goals";
+import Goals from "../pages/Goals/Goals";
 import AuthContext from "../context/Auth";
+import AppLayout from "../components/AppLayout/AppLayout";
+import NewGoals from "../pages/NewGoals";
 
 function Routing() {
   const { isLoggedin } = useContext(AuthContext);
 
   return (
     <Routes>
-      <Route path="" element={<Landing />} />
-      {isLoggedin && <Route path="/goals" element={<Goals />} />}
+      <Route element={<AppLayout />}>
+        <Route path="" element={<Landing />} />
+        {isLoggedin && (
+          <Route path="/goals" element={<Outlet />}>
+            <Route path="" element={<Goals />} />
+            <Route path="new" element={<NewGoals />} />
+          </Route>
+        )}
+      </Route>
     </Routes>
   );
 }
