da75eda0429e801916b364a92f725079a622b9d1
HyeonJu <hhjc017@gmail.com>
2022-08-19 15:12:35 +0900

    feat : LoadingSpinner 컴포넌트 추가

---
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./LoadingSpinner.module.css";
+
+function LoadingSpinner() {
+  return <div className={styles.spinner} />;
+}
+
+export default LoadingSpinner;
