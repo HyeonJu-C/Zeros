59905c71aaf2ea708cba4a8f7dd642002d0d473c
HyeonJu <hhjc017@gmail.com>
2022-08-19 17:07:00 +0900

    feat : 저축목표 delete 기능 추가

--- src/pages/Goals/GoalCard/GoalCard.tsx
+++ src/pages/Goals/GoalCard/GoalCard.tsx
@@ -1,8 +1,10 @@
 import { differenceInDays, format, parseJSON } from "date-fns";
-import React from "react";
+import React, { useState } from "react";
 import { GiMoneyStack as MoneyIcon } from "react-icons/gi";
+import { TbConfetti as ConfettiIcon } from "react-icons/tb";
+import Modal from "../../../components/Modal/Modal";
 import { auth } from "../../../services/firebase/config";
-import { GoalData } from "../../../services/firebase/database";
+import { deleteGoal, GoalData } from "../../../services/firebase/database";
 import styles from "./GoalCard.module.css";
 
 interface Props {
@@ -10,56 +12,95 @@ interface Props {
 }
 
 function GoalCard({ data }: Props) {
-  const { userName, goalSavings, targetDate, currentSavings, uid } = data;
+  const [isDeleted, setIsDeleted] = useState(false);
+  const [isModalVisible, setModalVisible] = useState(false);
+  const { userName, goalSavings, targetDate, currentSavings, uid, id } = data;
 
   const isAuthorized = auth.currentUser?.uid === uid;
-
-  const formattedGoalSavings = (+(goalSavings as string)).toLocaleString(
-    "en-US"
-  );
-
-  const acheiveRate =
-    (+(currentSavings as string) / +(goalSavings as string)) * 100;
 
   const parsedDate = parseJSON(targetDate as string);
   const formattedTargetDate = format(parsedDate, "yyyy년 MM월 dd일");
   const leftDays = differenceInDays(parsedDate, new Date());
 
+  const formattedGoalSavings = (
+    +(goalSavings as string) / 10000
+  ).toLocaleString("en-US");
+
+  const acheiveRate =
+    (+(currentSavings as string) / +(goalSavings as string)) * 100;
+
+  const onClickDelete = () => {
+    setModalVisible(true);
+  };
+
+  const onCancelDelete = () => {
+    setModalVisible(false);
+  };
+
+  const onConfirmDelete = async () => {
+    setModalVisible(false);
+    await deleteGoal(id as string);
+    setIsDeleted(true);
+  };
+
   return (
-    <article className={styles.card}>
-      <h1 className={styles.author}>{`${userName} 님의 저축 목표`}</h1>
-      <section className={styles.goalContainer}>
-        <div className={styles.iconContainer}>
-          <MoneyIcon color="#64c2a8" size="80%" />
-        </div>
-        <div className={styles.goal}>
-          <h2>Goals</h2>
-          <p>{`목표금액은 ${formattedGoalSavings}원 입니다.`}</p>
-          <p>{`목표 기한은 ${formattedTargetDate} 입니다.`}</p>
-        </div>
-      </section>
-      <section className={styles.barContainer}>
-        <h2>Acheive Rate</h2>
-        <div className={styles.parentBar}>
-          <div
-            className={styles.childBar}
-            style={{ width: `${acheiveRate}%` }}
-          />
-        </div>
-        <strong className={styles.currentGoal}>
-          <span>{`${acheiveRate}% 달성`}</span>
-          <span
-            className={styles.hilight}
-          >{`목표 기한까지 ${leftDays} 일`}</span>
-        </strong>
-      </section>
-      {isAuthorized && (
-        <>
-          <button type="button">rewrite</button>
-          <button type="button">delete</button>
-        </>
+    <>
+      {!isDeleted && (
+        <article className={styles.card}>
+          <h1 className={styles.author}>{`${userName} 님의 저축 목표`}</h1>
+          <section className={styles.goalContainer}>
+            <div className={styles.iconContainer}>
+              <MoneyIcon color="#64c2a8" size="80%" />
+            </div>
+            <div className={styles.goal}>
+              <h2>Goals</h2>
+              <p>{`목표금액은 ${formattedGoalSavings} 만원 입니다.`}</p>
+              <p>{`목표 기한은 ${formattedTargetDate} 입니다.`}</p>
+            </div>
+          </section>
+          <section className={styles.barContainer}>
+            <h2>Acheive Rate</h2>
+            <div className={styles.parentBar}>
+              <div
+                className={styles.childBar}
+                style={{ width: `${acheiveRate}%` }}
+              />
+            </div>
+            <strong className={styles.currentGoal}>
+              <span>{`${acheiveRate}% 달성 ${
+                acheiveRate >= 100 ? <ConfettiIcon /> : ""
+              }`}</span>
+              <span
+                className={styles.hilight}
+              >{`목표 기한까지 ${leftDays} 일`}</span>
+            </strong>
+          </section>
+          {isAuthorized && (
+            <section className={styles.buttonContainer}>
+              <button type="button" className={styles.save}>
+                저축하기
+              </button>
+              <button
+                type="button"
+                onClick={onClickDelete}
+                className={styles.delete}
+              >
+                삭제하기
+              </button>
+            </section>
+          )}
+        </article>
       )}
-    </article>
+      {isModalVisible && (
+        <Modal
+          title="Delete"
+          message="이 저축 목표를 삭제하시겠습니까?"
+          setModalVisible={setModalVisible}
+          onCancelClick={onCancelDelete}
+          onConfirmClick={onConfirmDelete}
+        />
+      )}
+    </>
   );
 }
 
--- src/services/firebase/database.ts
+++ src/services/firebase/database.ts
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-shadow */
 /* eslint-disable prettier/prettier */
-import { addDoc, collection, getDocs, Query } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDocs, Query } from "firebase/firestore";
 import { firestore } from "./config";
 
 const goalsCollection = collection(firestore, "goals");
@@ -24,4 +25,9 @@ export const postGoal = async (data: GoalData) => {
 export const getGoals = async (query: Query<object> = goalsCollection) => {
     const data = await getDocs(query);
     return data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+}
+
+export const deleteGoal = async (id: string) => {
+    const targetGoal = doc(firestore, "goals", id);
+    await deleteDoc(targetGoal)
 }
