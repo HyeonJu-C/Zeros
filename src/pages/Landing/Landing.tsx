91308215ff6df5cffecb4e1f2be56ba030f96955
HyeonJu <hhjc017@gmail.com>
2022-08-26 20:24:34 +0900

    feature : Explanation 컴포넌트 추가

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Explanation from "./Explanation/Explanation";
 import LoginForm from "./LoginForm/LoginForm";
 
 function Landing() {
   return (
     <section>
-      application explanation
+      <Explanation />
       <LoginForm />
     </section>
   );
